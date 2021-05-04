008476493917eed006a9de5c5d3d1481649d9187
Francisco Gustavo <hidalgofco520@gmail.com>
2021-05-04 18:23:30 -0500

    feat: add react-router-dom and config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ROUTER from './router';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Switch>
+          {ROUTER.map((route, idx) => (
+            <Route {...route} />
+          ))}
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/router/index.ts
@@ -0,0 +1,8 @@
+import { Home } from '../pages';
+
+const ROUTER = [
+  { exact: true, path: '/', component: Home },
+  { exact: true, path: '/polls', component: Home },
+];
+
+export default ROUTER;
